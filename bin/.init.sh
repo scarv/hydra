44f16ca5570ed57ef0a8e074fe111dbddf918fd2
phthinh <hung3@e.ntu.edu.sg>
2021-09-08 08:53:53 +0100

    add config.sh

---
+++ bin/.init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copyright (C) 2019 SCARV project <info@scarv.org>
+#
+# Use of this source code is restricted per the MIT license, a copy of which
+# can be found at https://opensource.org/licenses/MIT (or should be included
+# as LICENSE.txt within the associated archive or repository).
+
+export RISCV=~/Documents/riscv-toolchains/rv32imacb-ilp32
+export VIVADO_TOOL_DIR=/home/thinhpham/Xilinx/Vivado/2018.2
+source $VIVADO_TOOL_DIR/settings64.sh
+
